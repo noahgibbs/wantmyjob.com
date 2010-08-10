446ac13958a089d1514c2d8a8f75c2c0ec353500
Noah Gibbs <noah_gibbs@yahoo.com>
2010-08-09 23:31:10 -0700

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wantmyjob::Application.config.secret_token = '8bff4ed21e0035b24fba1f435ea3671f466c5454ff60e912c1bdfbb2aa8163c7802dffd1b364bfd7fc47cc0e90ea09386956f1275e1d1584a5f23763d76b75de'
