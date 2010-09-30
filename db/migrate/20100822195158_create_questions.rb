31158b0f87f2bc1bf592234211b1fbdcb980e0b2
Noah Gibbs <noah_gibbs@yahoo.com>
2010-09-29 18:49:04 -0700

    Add files for questions model

---
+++ db/migrate/20100822195158_create_questions.rb
@@ -0,0 +1,17 @@
+class CreateQuestions < ActiveRecord::Migration
+  def self.up
+    create_table :questions do |t|
+      t.integer :type
+      t.text :text, :limit => 3000
+      t.integer :created_by
+      t.boolean :approved
+      t.boolean :completed
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :questions
+  end
+end
