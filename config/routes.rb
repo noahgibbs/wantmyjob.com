31ae5084f58e46c2062c12f367c4188dea30a8eb
Noah Gibbs <noah_gibbs@yahoo.com>
2010-09-29 19:03:49 -0700

    Add routes, and fix link to registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Wantmyjob::Application.routes.draw do
+  resources :statistics
+
+  resources :profiles
+
+  resources :question_answers
+
+  resources :questions
+
+  get "registrations/quick"
+
   devise_for :users
 
   # The priority is based upon order of creation:
@@ -53,8 +63,4 @@ Wantmyjob::Application.routes.draw do
   root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
