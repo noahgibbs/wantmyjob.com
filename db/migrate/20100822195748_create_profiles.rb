dc1370b84ec7992a9a6d941246d856d512239d02
Noah Gibbs <noah_gibbs@yahoo.com>
2010-09-29 18:46:32 -0700

    Add files for profile model

---
+++ db/migrate/20100822195748_create_profiles.rb
@@ -0,0 +1,22 @@
+class CreateProfiles < ActiveRecord::Migration
+  def self.up
+    create_table :profiles do |t|
+      t.string :fullname
+      t.integer :birth_day
+      t.integer :birth_month
+      t.integer :birth_year
+      t.integer :gender
+      t.string :time_zone
+      t.text :address
+      t.string :country
+      t.string :state
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :profiles
+  end
+end
