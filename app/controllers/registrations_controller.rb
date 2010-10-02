2197db649ae2fc1d92e741d30ec7ec502ebcd0df
Noah Gibbs <noah_gibbs@yahoo.com>
2010-10-02 14:37:22 -0700

    Override registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,9 @@
-class RegistrationsController < ApplicationController
-  def quick
-    @profile = Profile.new
-  end
+class RegistrationsController < Devise::RegistrationsController
+
+  protected
+
+  #def after_update_path_for(resource)
+    # Return path
+    # "/path/for/after/editing/profile"
+  #end
 end
