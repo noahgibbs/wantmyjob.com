195b9e3c2312991a1ddbd618195b4fc71a596e3d
WWW user <www@ubuntu.(none)>
2010-10-03 10:50:20 -0700

    Bugfix to migration

--- db/migrate/20101003173519_create_companies.rb
+++ db/migrate/20101003173519_create_companies.rb
@@ -3,7 +3,7 @@ class CreateCompanies < ActiveRecord::Migration
     create_table :companies do |t|
       t.string :name, :limit => 200
       t.text :address
-      t.apply_email :string, :limit => 100
+      t.string :apply_email, :limit => 100
 
       t.timestamps
     end
