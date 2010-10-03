a70bab1d23da845cd706aa7631e3d10df755616d
Noah Gibbs <noah_gibbs@yahoo.com>
2010-10-03 10:40:31 -0700

    Add portal action to home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,18 @@ class HomeController < ApplicationController
   def index
     @big_logo = true
   end
+
+  # Where to next on the site...
+  def portal
+    unless current_user.profile
+      # Make a new empty profile for the user
+      current_user.create_profile
+      current_user.save!
+      current_user.profile.save!
+    end
+
+    # Check to see if the user has entered any current jobs
+
+    raise "Unimplemented"
+  end
 end
