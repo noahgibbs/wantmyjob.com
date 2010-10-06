741883dda80609f98f449136b001836655e13d5e
Noah Gibbs <noah_gibbs@yahoo.com>
2010-10-05 18:14:40 -0700

    Formatting fix

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   #def after_update_path_for(resource)
-    # Return path
-    # "/path/for/after/editing/profile"
+  #  Return path
+  #  "/path/for/after/editing/profile"
   #end
 end
