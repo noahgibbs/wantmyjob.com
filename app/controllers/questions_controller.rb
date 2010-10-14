ca35e8b5f837447e16663d47d1806c326002ed78
Noah Gibbs <noah_gibbs@yahoo.com>
2010-10-13 23:42:40 -0700

    Decent_exposure-ify this controller and its views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,22 @@
 class QuestionsController < ApplicationController
   before_filter :authenticate_user!
-  before_filter :requires_admin, :except => [:answer, :answer_post,
-                                             :enter, :enter_post]
+  before_filter :requires_admin,
+                  :except => [:answer, :answer_post,
+                              :enter, :enter_post]
 
   respond_to :html
+
+  expose(:questions) { Question.scoped }
+  expose(:question)
+  expose(:answers) do
+    if params[:answers]
+      params[:answers].select {|s| !s[:text].empty?}.map do |a|
+        question << QuestionAnswer.new(a)
+      end
+    else
+      (1..Question::MAX_ANSWERS).map { QuestionAnswer.new }
+    end
+  end
 
   def answer
   end
@@ -11,83 +24,42 @@ class QuestionsController < ApplicationController
   def answer_post
   end
 
-  def enter
-    @question = Question.new
-    @answers = (1..Question::MAX_ANSWERS).map { QuestionAnswer.new }
-  end
-
   def enter_post
-    question = Question.new(params[:question])
-    answers = params[:answer].map {|a| QuestionAnswer.new(a)}
+    q = question
+    ans = answers
+    ActiveRecord::Base.transaction do
+      q.save!
+      ans.each { |a| a.save! }
+    end
+    redirect_to questions_path
   end
 
   # GET /questions
-  # GET /questions.xml
   def index
-    @questions = Question.all
-
-    respond_with @questions
-  end
-
-  # GET /questions/1
-  # GET /questions/1.xml
-  def show
-    @question = Question.find(params[:id])
-
-    respond_with @question
-  end
-
-  # GET /questions/new
-  # GET /questions/new.xml
-  def new
-    @question = Question.new
-
-    respond_with @question
-  end
-
-  # GET /questions/1/edit
-  def edit
-    @question = Question.find(params[:id])
+    respond_with questions
   end
 
   # POST /questions
-  # POST /questions.xml
   def create
-    @question = Question.new(params[:question])
-
-    respond_to do |format|
-      if @question.save
-        format.html { redirect_to(@question, :notice => 'Question was successfully created.') }
-        format.xml  { render :xml => @question, :status => :created, :location => @question }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @question.errors, :status => :unprocessable_entity }
-      end
+    if question.save
+      redirect_to(question, :notice => 'Question was successfully created.')
+    else
+      render :action => "new"
     end
   end
 
   # PUT /questions/1
-  # PUT /questions/1.xml
   def update
-    @question = Question.find(params[:id])
-
-    respond_to do |format|
-      if @question.update_attributes(params[:question])
-        format.html { redirect_to(@question, :notice => 'Question was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @question.errors, :status => :unprocessable_entity }
-      end
+    if question.update_attributes(params[:question])
+      redirect_to(question, :notice => 'Question was successfully updated.')
+    else
+      render :action => "edit"
     end
   end
 
   # DELETE /questions/1
-  # DELETE /questions/1.xml
   def destroy
-    @question = Question.find(params[:id])
-    @question.destroy
-
-    respond_with nil, :location => questions_url, :status => :ok
+    question.destroy
+    respond_with question
   end
 end
