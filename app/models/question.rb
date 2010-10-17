17873af8a279ac3ccc713cc4e0fcc5bbc1f1dc31
Noah Gibbs <noah_gibbs@yahoo.com>
2010-10-16 21:44:23 -0700

    SQL snippet (unused?) for next perfect co. question, and useful new scopes

--- app/models/question.rb
+++ app/models/question.rb
@@ -19,4 +19,25 @@ class Question < ActiveRecord::Base
   # An Employer question can be about a previous employer,
   # or about your perfect company to work for.
   EMPLOYER_QUESTION = 1
+
+  scope :random_order, order("RAND()")
+  scope :verified, where(:verified => true, :completed => true,
+                         :soft_deleted => false)
+
+  # This is actually done by answer type, not question type.  It
+  # returns a Question, though, so it winds up having to be a
+  # scope on Question, not Answer.
+
+  # This is going to be *SO SLOW*.  But I'll optimize after I know
+  # better what a right answer will look like.
+  scope :next_perfect_company_question_for_profile lambda {|profile|
+    joins("LEFT JOIN " +
+          "(select * from answers WHERE " +
+            "answer_type = #{Answer::PERFECT_COMPANY_ANSWER} AND " +
+            "profile_id = #{profile.id.to_i}) AS a " +
+          "ON questions.id = a.question_id").
+    where("a.answer_type IS NULL").
+    order("questions.id")
+  }
+
 end
