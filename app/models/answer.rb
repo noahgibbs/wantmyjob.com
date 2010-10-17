dac9777a830b21fd67ff5feacd8fdbfe32db97c9
Noah Gibbs <noah_gibbs@yahoo.com>
2010-10-16 21:43:41 -0700

    Add profile_id back, and remove unused scopes

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,5 @@
 class Answer < ActiveRecord::Base
+  belongs_to :profile
   belongs_to :job
   belongs_to :question
 
@@ -12,20 +13,6 @@ class Answer < ActiveRecord::Base
 
   scope :for_profile_id, lambda {|pid| where(:profile_id => pid)}
   scope :for_company_id, lambda {|cid| where(:company_id => cid)}
-
-  scope :last_answer, lambda {|ans_type|
-    where(:answer_type => ans_type).
-      order("question_id DESC, company_id ASC").
-      limit(1)
-  }
-
-  def self.last_perfect_answer
-    last_answer(PERFECT_COMPANY_ANSWER)
-  end
-
-  def self.last_company_answer
-    last_answer(COMPANY_ANSWER)
-  end
 
   def profile_id
     job.profile_id
