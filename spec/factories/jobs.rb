2b18bb9d7f7f36987ec83a2333edffd79e8230d4
WWW user <www@ubuntu.(none)>
2010-10-19 14:57:00 -0700

    Fix factory file, with comments about why I hacked around a standard method

--- spec/factories/jobs.rb
+++ spec/factories/jobs.rb
@@ -1,11 +1,11 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
-Factory.define :job do |f|
-  f.title "Master of Ceremonies"
-  f.start_date Time.parse("July 3, 1991")
-  f.end_date Time.parse("May 8, 1995")
-  #f.work_site { f.association :work_site }
-  #f.profile { f.association :profile }
-  f.work_site { Factory.build :work_site }
-  f.profile { Factory.build :profile }
+Factory.define :job do |j|
+  j.title "Master of Ceremonies"
+  j.start_date Time.parse("July 3, 1991")
+  j.end_date Time.parse("May 8, 1995")
+  #j.association :work_site  # No good - won't work with attributes_for
+  #j.association :profile    # No good - won't work with attributes_for
+  j.work_site { Factory.build(:work_site) }
+  j.profile { Factory.build(:profile) }
 end
