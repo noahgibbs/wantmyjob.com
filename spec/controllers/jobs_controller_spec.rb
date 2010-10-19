129a8f8894d52179956ed42f8160d5dd17ba092e
Noah Gibbs <noah_gibbs@yahoo.com>
2010-10-19 07:47:34 -0700

    Started working on testing devise properly

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -1,6 +1,20 @@
 require 'spec_helper'
+require 'devise/test_helpers'
 
 describe JobsController do
+  let(:usermodel) do
+    stub_model(User).as_new_record
+  end
+
+  before do
+    assign(:user, usermodel)
+
+    # Devise provides resource and resource_name helpers and
+    # mappings so stub them here.
+    @view.stub(:resource).and_return(@user)
+    @view.stub(:resource_name).and_return('user')
+    @view.stub(:devise_mapping).and_return(Devise.mappings[:user])
+  end
 
   def mock_job(stubs={})
     (@mock_job ||= mock_model(Job).as_null_object).tap do |job|
