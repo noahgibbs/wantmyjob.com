3866777a57a8fa216169af32cd87e99a9a48528b
WWW user <www@ubuntu.(none)>
2010-10-19 11:21:58 -0700

    Fix the first failure of many

--- spec/views/jobs/edit.html.erb_spec.rb
+++ spec/views/jobs/edit.html.erb_spec.rb
@@ -2,10 +2,21 @@ require 'spec_helper'
 
 describe "jobs/edit.html.erb" do
   before(:each) do
+    @profile = assign(:profile, stub_model(Profile,
+      :new_record? => false,
+      :fullname => "Harry Q. Bovik, Esq."
+    ))
+    @work_site = assign(:work_site, stub_model(WorkSite,
+      :new_record? => false,
+      :company_name => "BogoDyne"
+    ))
     @job = assign(:job, stub_model(Job,
       :new_record? => false,
-      :name => "MyString",
-      :foozle_count => 1
+      :title => "Master of Ceremonies",
+      :start_date => Time.parse("July 3, 1991"),
+      :end_date => Time.parse("May 8, 1995"),
+      :work_site => @work_site,
+      :profile => @profile
     ))
   end
 
@@ -14,8 +25,7 @@ describe "jobs/edit.html.erb" do
 
     # Run the generator again with the --webrat-matchers flag if you want to use webrat matchers
     assert_select "form", :action => job_path(@job), :method => "post" do
-      assert_select "input#job_name", :name => "job[name]"
-      assert_select "input#job_foozle_count", :name => "job[foozle_count]"
+      assert_select "input#job_title", :name => "job[title]"
     end
   end
 end
