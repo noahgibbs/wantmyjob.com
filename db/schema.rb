f2522a6f0ca96794830a922b33ed527361bb2eb0
Noah Gibbs <noah_gibbs@yahoo.com>
2010-10-21 02:10:14 -0700

    Make job_id column nullable in answers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101016063052) do
+ActiveRecord::Schema.define(:version => 20101021090747) do
 
   create_table "answers", :force => true do |t|
-    t.integer "job_id",                     :null => false
+    t.integer "job_id"
     t.integer "answer_type",                :null => false
     t.integer "question_id",                :null => false
     t.integer "data1",       :default => 0, :null => false
