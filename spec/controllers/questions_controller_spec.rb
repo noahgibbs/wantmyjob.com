1d932602c1e6c1d99b672437ccd357d887e21345
WWW user <www@ubuntu.(none)>
2010-10-25 23:16:46 -0700

    Test basic answer_post action

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,10 +3,6 @@ require 'spec_helper'
 describe QuestionsController do
   include AppSpecHelpers
 
-  def mock_question(stubs={})
-    mock_ar_object("question", stubs)
-  end
-
   describe "GET enter" do
     it "doesn't fail completely" do
       get :enter
@@ -14,10 +10,44 @@ describe QuestionsController do
   end
 
   describe "POST enter_post" do
-    # Real test goes here
-
     it "should redirect to the portal" do
       post :enter_post, :jobs => [{}]
+      should redirect_to(:controller => :home, :action => :portal)
+    end
+  end
+
+  describe "GET answer" do
+    let(:profile) { Factory(:profile) }
+
+    before do
+      mock_user.stub(:profile) { profile }
+      profile.stub(:next_question) { [nil, nil, nil] }
+    end
+
+    it "redirects to enter questions if no next question is found" do
+      get :answer
+      should redirect_to(:action => :enter)
+    end
+  end
+
+  describe "POST answer_post" do
+    let(:profile) { Factory(:profile) }
+
+    before do
+      mock_user.stub(:profile) { profile }
+    end
+
+    it "redirects to the portal with no sub-keys" do
+      ans = mock_model(Answer)
+      Answer.should_receive(:new).once.with("data1" => 271, "data2" => 35,
+          "data3" => 0, "data4" => 0, "profile_id" => 7,
+          "answer_type" => 1, "question_id" => 4,
+          "job_id" => 6).and_return(ans)
+      ans.should_receive(:save!).once
+      post :answer_post,
+        :answer => { :data1 => "271", :data2 => "35", :data3 => "",
+                     :data4 => "0", :profile_id => 7,
+                     :answer_type => 1, :question_id => 4, :job_id => 6 }
       should redirect_to(:controller => :home, :action => :portal)
     end
   end
