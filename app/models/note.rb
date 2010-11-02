7bef27a940ae125c8f02e951894827640ad44650
WWW user <www@ubuntu.(none)>
2010-11-02 16:10:40 -0700

    Fixes to note model

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,11 +1,11 @@
 class Note < ActiveRecord::Base
-  NOTE_TYPE_DAILY_FEED_STATS = 1
+  DAILY_FEED_STATS = 1
 
-  scope :daily_feed, where(:note_type => NOTE_TYPE_DAILY_FEED_STATS).
+  scope :daily_feed, where(:note_type => DAILY_FEED_STATS).
         order("updated_at DESC")
 
   def self.generate_daily_feed_stats
-    n = Note.new(:title => "Feed Stats for #{Date.today}",
+    n = Note.new :title => "Feed Stats for #{Date.today}",
                  :note_type => DAILY_FEED_STATS,
                  :body => <<END
 Devise Users: #{User.count}
@@ -13,8 +13,9 @@ Profiles: #{Profile.count}
 Demo Profiles: #{Profile.where(:demo => true).count}
 Valid Questions: #{Question.verified.count}
 Waiting Questions: #{Question.unverified.count}
+Jobs: #{Job.count}
 Answers: #{Answer.count}
-END)
+END
     n.save!
   end
 end
