748bcfc11fae7f213cd878b55c2def8b2d7c24a8
WWW user <www@ubuntu.(none)>
2010-11-02 16:20:06 -0700

    Add HTML line breaks

--- app/models/note.rb
+++ app/models/note.rb
@@ -8,13 +8,13 @@ class Note < ActiveRecord::Base
     n = Note.new :title => "Feed Stats for #{Date.today}",
                  :note_type => DAILY_FEED_STATS,
                  :body => <<END
-Devise Users: #{User.count}
-Profiles: #{Profile.count}
-Demo Profiles: #{Profile.where(:demo => true).count}
-Valid Questions: #{Question.verified.count}
-Waiting Questions: #{Question.unverified.count}
-Jobs: #{Job.count}
-Answers: #{Answer.count}
+Devise Users: #{User.count} <br/>
+Profiles: #{Profile.count} <br/>
+Demo Profiles: #{Profile.where(:demo => true).count} <br/>
+Valid Questions: #{Question.verified.count} <br/>
+Waiting Questions: #{Question.unverified.count} <br/>
+Jobs: #{Job.count} <br/>
+Answers: #{Answer.count} <br/>
 END
     n.save!
   end
