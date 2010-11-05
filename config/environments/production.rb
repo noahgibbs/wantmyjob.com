3d975c093f5bbf28703c633b2609abdb49495068
WWW user <www@ubuntu.(none)>
2010-11-04 22:38:10 -0700

    Check nils aggressively

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,8 @@ Wantmyjob::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
+  config.whiny_nils = true
+
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
