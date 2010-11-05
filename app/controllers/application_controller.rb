463d3c9e6f2e84aa07a8ce57512e95fa605e02ab
WWW user <www@ubuntu.(none)>
2010-11-04 22:32:53 -0700

    Add malformed URL helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
   def requires_admin
     redirect_to "/" unless current_user.admin
   end
+
+  def malformed_url_error
+    render :text => "Bad URL!  Did you paste it wrong, maybe?", :status => 404
+  end
 end
