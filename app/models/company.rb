4bd5e603dd57d0909a93513747f444b32f647bd9
Noah Gibbs <noah_gibbs@yahoo.com>
2010-11-23 21:45:37 -0800

    Methods to use search codes

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,31 @@
 class Company < ActiveRecord::Base
   has_many :work_sites
+
+  IGNORED_WORDS = {
+    "inc" => true,
+    "co" => true,
+    "company" => true,
+    "corp" => true,
+    "corporation" => true,
+    "incorporated" => true,
+    "llc" => true,
+    "limited" => true,
+    "group" => true,
+    "com" => true,
+    "technology" => true,
+    "systems" => true,
+    "center" => true,
+  }
+
+  def generate_search_code
+    return if search_code && !company_name_changed?
+    self.search_code = Company.generate_search_code(company_name)
+  end
+
+  def self.generate_search_code(company_name)
+    words = company_name.split(/(\w+)/).select {|w| w =~ /\w+/}.map(&:downcase)
+    words = words.select {|w| !IGNORED_WORDS.include?(w)}
+
+    words.join(" ")
+  end
 end
--- app/models/work_site.rb
+++ app/models/work_site.rb
@@ -2,7 +2,14 @@ class WorkSite < ActiveRecord::Base
   belongs_to :company
   has_many :jobs
 
+  before_save :generate_search_code
+
   def checked_company_name
     company_id ? company.company_name : company_name
+  end
+
+  def generate_search_code
+    return if search_code && !company_name_changed?
+    self.search_code = Company.generate_search_code(company_name)
   end
 end
