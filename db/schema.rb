45b420e9d575a79b0030ddd3f6de1c5a82a1ab46
Noah Gibbs <noah_gibbs@yahoo.com>
2010-12-12 17:03:31 -0800

    Add migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101202135847) do
+ActiveRecord::Schema.define(:version => 20101213002656) do
 
   create_table "answers", :force => true do |t|
     t.integer "job_id"
@@ -67,6 +67,13 @@ ActiveRecord::Schema.define(:version => 20101202135847) do
     t.datetime "updated_at"
   end
 
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "profiles", :force => true do |t|
     t.string   "fullname"
     t.integer  "birth_day"
@@ -109,6 +116,23 @@ ActiveRecord::Schema.define(:version => 20101202135847) do
     t.float    "float_value"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context"
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
   end
 
   create_table "users", :force => true do |t|
